5c9edd5e05fd6b641d53f99d0a38ffa8d182ea9f
albertsube <albert.sune.belmonte@gmail.com>
2023-12-18 17:59:41 +0100

    ESlint + Prettier

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -1,15 +1,15 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const recipeController = require("../controllers/recipeController")
+const recipeController = require('../controllers/recipeController')
 
-router.get("/", recipeController.getAllRecipes)
+router.get('/', recipeController.getAllRecipes)
 
-router.post("/", recipeController.createRecipe)
+router.post('/', recipeController.createRecipe)
 
-router.put("/:id", recipeController.updateRecipe)
+router.put('/:id', recipeController.updateRecipe)
 
-router.get("/:id", recipeController.getRecipeById)
+router.get('/:id', recipeController.getRecipeById)
 
-router.delete("/:id", recipeController.deleteRecipe)
+router.delete('/:id', recipeController.deleteRecipe)
 
 module.exports = router
