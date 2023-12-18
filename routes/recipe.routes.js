c88b5186f322fa513b97bdec3020bd19dc33d6c6
albertsube <albert.sune.belmonte@gmail.com>
2023-12-18 17:39:56 +0100

    Update routes

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -1,15 +1,15 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const recipeController = require('../controllers/recipeController')
+const recipeController = require("../controllers/recipeController")
 
-router.get('/', recipeController.getAllRecipes);
+router.get("/", recipeController.getAllRecipes)
 
-router.post('/create', recipeController.createRecipe);
+router.post("/", recipeController.createRecipe)
 
-router.put('/update/:id', recipeController.updateRecipe);
+router.put("/:id", recipeController.updateRecipe)
 
-router.get('/:id', recipeController.getRecipeById);
+router.get("/:id", recipeController.getRecipeById)
 
-router.delete('/delete/:id', recipeController.deleteRecipe);
+router.delete("/:id", recipeController.deleteRecipe)
 
-module.exports = router;
+module.exports = router
