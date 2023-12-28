725885e0076936d0e81e3a960a635f0ff84279dc
albertsube <albert.sune.belmonte@gmail.com>
2023-12-28 22:54:19 +0100

    User seeding

--- seed.js
+++ seed.js
@@ -5,17 +5,30 @@ const db = require('./config/db')
 
 //Load models
 const Recipe = require('./models/recipe.model')
+const User = require('./models/user.model')
 
 //Connect to db
 db().then()
 
 // Read the JSON files
 const recipes = JSON.parse(fs.readFileSync('./data/recipe.data.json', 'utf-8'))
+const users = JSON.parse(fs.readFileSync('./data/user.data.json', 'utf-8'))
 
 // Import sample data in DB
 const importData = async () => {
   try {
-    await Recipe.create(recipes)
+    await User.create(users)
+
+    // Assign random user
+    const createdUsers = await User.find()
+    const recipesWithUser = recipes.map((recipe) => {
+      const randomIndex = Math.floor(Math.random() * createdUsers.length)
+      const author = createdUsers[randomIndex]._id
+      return { ...recipe, author }
+    })
+
+    await Recipe.create(recipesWithUser)
+
     console.log(`Data successfully imported`.green.inverse)
   } catch (err) {
     console.log(err)
@@ -27,6 +40,7 @@ const importData = async () => {
 // Delete the data from DB
 const deleteData = async () => {
   try {
+    await User.deleteMany()
     await Recipe.deleteMany()
     console.log(`Data successfully deleted`.red.inverse)
   } catch (err) {
