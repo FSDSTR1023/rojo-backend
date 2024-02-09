c8b1b240959f288b9ffaf65059ccab5273bbd72e
albertsube <albert.sune.belmonte@gmail.com>
2024-02-09 20:50:29 +0100

    Filter by difficulty and preparation time

--- middlewares/recipe.middleware.js
+++ middlewares/recipe.middleware.js
@@ -1,7 +1,30 @@
+const { DIFFICULTY, PREPARATION_TIME, CATEGORIES } = require('../constants/recipe.js')
+
 function filters(req, res, next) {
-  const { author } = req.query
-  res.filters = { ...(author && { author }) }
+  const { author, difficulty, preparationTime } = req.query
+  res.filters = {
+    ...(author && { author }),
+    ...(DIFFICULTY.includes(difficulty) && { difficulty }),
+    ...(PREPARATION_TIME.includes(preparationTime) && { preparationTime }),
+  }
   next()
 }
 
 module.exports = { filters }
+
+// Filters
+// Category
+
+// Title
+
+// Ingredients
+
+// Dificulty
+
+// Preparation Time
+
+// Rating
+
+// Order
+// Rating
+// Date
