98382af6af7f490d873d63d35c07eec339541ad0
albertsube <albert.sune.belmonte@gmail.com>
2024-02-09 21:22:49 +0100

    Filter by category

--- middlewares/recipe.middleware.js
+++ middlewares/recipe.middleware.js
@@ -1,12 +1,20 @@
 const { DIFFICULTY, PREPARATION_TIME, CATEGORIES } = require('../constants/recipe.js')
 
 function filters(req, res, next) {
-  const { author, difficulty, preparationTime } = req.query
+  const { author, difficulty, preparationTime, categories } = req.query
+
+  const verifiedCategories =
+    typeof req.query.categories === 'object'
+      ? categories.filter((category) => CATEGORIES.includes(category))
+      : CATEGORIES.includes(categories) && [categories]
+
   res.filters = {
     ...(author && { author }),
     ...(DIFFICULTY.includes(difficulty) && { difficulty }),
     ...(PREPARATION_TIME.includes(preparationTime) && { preparationTime }),
+    ...(verifiedCategories && { categories: { $all: verifiedCategories } }),
   }
+  console.log(res.filters)
   next()
 }
 
@@ -18,10 +26,6 @@ module.exports = { filters }
 // Title
 
 // Ingredients
-
-// Dificulty
-
-// Preparation Time
 
 // Rating
 
