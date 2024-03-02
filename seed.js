cf1ecf16fed425f3c4cbb79df0f6b5a5c1c7cb83
albertsube <albert.sune.belmonte@gmail.com>
2024-03-02 15:58:03 +0100

    Revert seed

--- seed.js
+++ seed.js
@@ -1,15 +1,84 @@
-const db = require('./config/db')
+const dotenv = require('dotenv')
+const fs = require('fs')
 const colors = require('colors')
-const deleteData = require('./mocks/DeleteData.js')
-const importData = require('./mocks/ImportData.js')
+const db = require('./config/db')
+const bcrypt = require('bcrypt')
+const random = require('./utils/random.js')
+
+//Load models
+const Recipe = require('./models/recipe.model')
+const User = require('./models/user.model')
 
 //Connect to db
 db().then()
 
 // Import sample data in DB
-const importDataDB = async () => {
+const importData = async () => {
   try {
-    await importData()
+    // Read the JSON files
+    const recipes = JSON.parse(fs.readFileSync('./data/recipe.data.json', 'utf-8'))
+    const users = JSON.parse(fs.readFileSync('./data/user.data.json', 'utf-8'))
+    const opinions = JSON.parse(fs.readFileSync('./data/opinion.data.json', 'utf-8'))
+
+    // Encrypt passwords
+    const saltRounds = 10
+    const encryptedUsers = users.map((user) => {
+      const encryptedPassword = bcrypt.hashSync(user.password, saltRounds)
+      return { ...user, password: encryptedPassword }
+    })
+
+    // Create ussers
+    await User.create(encryptedUsers)
+    const createdUsers = await User.find()
+
+    // Add random followers
+    const usersWithFollowers = createdUsers.map((user) => {
+      const otherUsers = createdUsers.filter((u) => u._id !== user._id)
+      const numFollowers = random.getRandomIntInclusive(3, 6)
+      const randomFollowers = random.elements(otherUsers, numFollowers).map((e) => e._id)
+      return { id: user._id, followers: randomFollowers }
+    })
+
+    for (const user of usersWithFollowers) {
+      for (const follower of user.followers) {
+        await Promise.all([
+          User.findByIdAndUpdate(follower, { $addToSet: { following: user.id } }, { new: true }),
+          User.findByIdAndUpdate(user.id, { $addToSet: { followers: follower } }, { new: true }),
+        ])
+      }
+    }
+
+    //Assign random user to opinions
+    const opinionsWithUser = opinions.map((opinion) => {
+      const user = random.element(createdUsers)._id
+      return { ...opinion, user }
+    })
+
+    // Assign random user and opinions to recipes
+    const recipesWithUser = recipes.map((recipe) => {
+      const author = random.element(createdUsers)._id
+      const numOpinions = random.getRandomIntInclusive(5, 10)
+      const randomOpinions = random.elements(opinionsWithUser, numOpinions)
+      const rating = randomOpinions.reduce((acc, opinion) => acc + opinion.rating, 0) / randomOpinions.length
+      return { ...recipe, author, opinions: randomOpinions, rating }
+    })
+
+    // Create recipes
+    const createdRecipes = await Recipe.create(recipesWithUser)
+
+    // Assign recipes to authors
+    for (const recipe of createdRecipes) {
+      const author = recipe.author
+      await User.findByIdAndUpdate(author, { $addToSet: { recipes: recipe } }, { new: true })
+    }
+
+    // Assign random favorite recipes
+    for (const user of createdUsers) {
+      const numFavRecipes = random.getRandomIntInclusive(5, 10)
+      const favRecipes = random.elements(createdRecipes, numFavRecipes).map((r) => r._id)
+      await User.findByIdAndUpdate(user._id, { favRecipes }, { new: true })
+    }
+
     console.log(`Data successfully imported`.green.inverse)
   } catch (err) {
     console.log(err)
@@ -19,9 +88,10 @@ const importDataDB = async () => {
 }
 
 // Delete the data from DB
-const deleteDataDB = async () => {
+const deleteData = async () => {
   try {
-    await deleteData()
+    await User.deleteMany()
+    await Recipe.deleteMany()
     console.log(`Data successfully deleted`.red.inverse)
   } catch (err) {
     console.log(err)
@@ -33,10 +103,10 @@ const deleteDataDB = async () => {
 // Select operation
 switch (process.argv[2]) {
   case '-i':
-    importDataDB().then()
+    importData().then()
     break
   case '-d':
-    deleteDataDB().then()
+    deleteData().then()
     break
   default:
     console.log(`Wrong parameter assigned`.yellow.inverse)
